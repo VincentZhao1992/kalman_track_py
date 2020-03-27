9b0d700f2cc5144813bdad1197f41f7498834038
VincentZhao1992 <zwz_tjut@163.com>
2020-03-27 12:26:35 +0800

    read imgs test

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
+import os
+
 import cv2
 
+data_root = '/home/zwz/workspace/data'
 
-def printinfo(str):
-    print(cv2.getVersionMajor())
+pic_root = os.path.join(data_root, 'uav0000086_00000_v')
 
+img_path = os.listdir(pic_root)
+img_path.sort()
 
-if __name__ == "__main__":
-    printinfo("hello")
+imgs = [os.path.join(pic_root, i) for i in img_path]
+
+for img in imgs:
+    frame = cv2.imread(img)
+    cv2.imshow('dis', frame)
+    k = cv2.waitKey(10)
+    if k == 27:
+        break
+cv2.destroyAllWindows()
